97b0cd02fa4054ab9a0ecf05c4ce80ddd06be68c
Darren Burns <darrenb900@gmail.com>
2017-04-09 22:05:14 +0100

    Home and end key support

--- kilo.c
+++ kilo.c
@@ -25,7 +25,12 @@ enum editor_key {
   ARROW_LEFT = 1000,
   ARROW_RIGHT,
   ARROW_UP,
-  ARROW_DOWN
+  ARROW_DOWN,
+  PAGE_UP,
+  PAGE_DOWN,
+  HOME,
+  END,
+  DELETE
 };
 
 /*** append buffer ***/
@@ -176,16 +181,39 @@ int read_key() {
     if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
     if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
 
+    // Parse the VT100 control sequence
     if (seq[0] == '[') {
-      switch (seq[1]) {
-        case 'A': return ARROW_UP;
-        case 'B': return ARROW_DOWN;
-        case 'C': return ARROW_RIGHT;
-        case 'D': return ARROW_LEFT;
+      if (seq[1] >= '0' && seq[1] <= '9') {
+        if (read(STDIN_FILENO, &seq[2], 1) != 1) {
+          return '\x1b';
+        }
+        if (seq[2] == '~') {
+          switch (seq[1]) {
+            case '1': return HOME;
+            case '4': return END;
+            case '5': return PAGE_UP;
+            case '6': return PAGE_DOWN;
+            case '7': return HOME;
+            case '8': return END;
+          }
+        }
+      } else if (seq[0] == 'O') {
+        switch (seq[1]) {
+          case 'H': return HOME;
+          case 'F': return END;
+        }
+      } else {
+        switch (seq[1]) {
+          case 'A': return ARROW_UP;
+          case 'B': return ARROW_DOWN;
+          case 'C': return ARROW_RIGHT;
+          case 'D': return ARROW_LEFT;
+          case 'H': return HOME;
+          case 'F': return END;
+        }
       }
-
-      return '\x1b';
     }
+    return '\x1b';
   }
   return c;
 }
@@ -294,6 +322,23 @@ void read_and_process_key() {
       clear_screen_raw();
       exit(0);
       break;
+
+    case HOME:
+      E.current_x = 0;
+      break;
+
+    case END:
+      E.current_y = E.screen_rows - 1;
+      break;
+
+    case PAGE_UP:
+    case PAGE_DOWN: {
+      int times = E.screen_rows;
+      while (times--) {
+        move_cursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+      }
+    }
+    break;
     
     // Cursor movement
     case ARROW_UP:
